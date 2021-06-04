84af8cb1ed03d099b1f71c9480d66e5085aa62f7
Simon-Oliver <stauffer.simon@outlook.com>
2021-06-04 20:58:17 +0200

    exploring sql

--- sqlite.py
+++ sqlite.py
@@ -4,8 +4,14 @@ conn = sqlite3.connect("test.db")
 # Create Cursor
 cur = conn.cursor()
 
-comm = "SELECT emp_id FROM employees"
+comm = "SELECT emp_id, name FROM employees"
+tabl = """SELECT name FROM sqlite_schema
+WHERE type='table'
+ORDER BY name"""
 desc = "PRAGMA table_info(employees)"
+emp_epp = "SELECT employees.emp_id, employees.name, epp.epp, ROUND(epp.strike_price,2), ROUND(epp.epp * epp.strike_price,2) as Val FROM employees INNER JOIN epp ON employees.emp_id = epp.emp_id"
 # cur.execute(desc)
-cur.execute(comm)
+cur.execute(emp_epp)
 print(cur.fetchall())
+
+conn.close()
