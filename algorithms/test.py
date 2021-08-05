48fef063987411c6b83f7d80be3be5fce21feb3c
Simon-Oliver <stauffer.simon@outlook.com>
2021-08-05 20:07:06 +0200

    exploring algo

--- algorithms/test.py
+++ algorithms/test.py
@@ -1,1 +1,16 @@
-print("Test")
+class Game:
+    def __init__(self, start, end):
+        self.range = [*range(start, end + 1, 1)]
+        self.count = 0
+
+    def split_list(a_list):
+        half = len(a_list)//2
+        return a_list[:half], a_list[half:]
+
+    def play(self):
+        print(len(self.range)//2)
+    #    self.split_list(self.range)
+
+
+game1 = Game(1, 20)
+game1.play()
